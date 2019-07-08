d9f3d564b8e260cad7bdb6bd38f90919f15412e5
irfan khoirul <irfankhoirul@gmail.com>
2019-07-08 16:00:00 +0700

    end

--- app/src/main/java/com/tokopedia/training/MainActivity.kt
+++ app/src/main/java/com/tokopedia/training/MainActivity.kt
@@ -3,7 +3,9 @@ package com.tokopedia.training
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text_title.text = getString(R.string.greeting_training)
+        text_title.text = getString(R.string.greeting_new)
 
         image_view.setImageResource(R.drawable.ic_launcher_foreground)
+
+        Glide.with(this)
+            .load("http://pngimg.com/uploads/android_logo/android_logo_PNG32.png")
+            .into(image_view)
 
         button_calculate.setOnClickListener {
             if (!TextUtils.isEmpty(number_1.text) && !TextUtils.isEmpty(number_2.text)) {
